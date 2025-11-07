402495b0729b8745139fa67a2d069882304c1411
rehanajaib04-commits <rehanajaib04@gmail.com>
2025-11-07 17:11:14 +0000

    act3 (#2)

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -1,16 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Pressable, StyleSheet, ScrollView, Text, View } from 'react-native';
 import Screen from '../layout/Screen';
+import initialModules from '../../data/modules.js';
 
 const ModuleListScreen = () => {
+  // Initialisation
+  const modules = initialModules; 
+//state
+ //handlers
+  const handleSelect = () => alert("Item selected");
+ //view 
+
+
   return (
     <Screen>
-      <Text>List</Text>
+      <ScrollView style={styles.container}>
+        {modules.map(module => (
+          < Pressable key={module.ModuleCode}  onPress={handleSelect}>
+            <View style={styles.item}>
+              <Text style={styles.text}>
+                {module.ModuleCode} {module.ModuleName}
+              </Text>
+            </View>
+          </Pressable>
+        ))}
+      </ScrollView>
     </Screen>
   );
 };
 
 const styles = StyleSheet.create({
-  });
+  container: { 
+  },
+  item: { 
+    paddingVertical: 15,
+    borderTopWidth: 1,
+    borderColor: 'lightgrey',
+  },
+  text: { 
+    fontSize: 16,
+  },
+}); 
 
 export default ModuleListScreen;
