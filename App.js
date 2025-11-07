5eca99868bc69712f3219ca4bf43501efdc467e6
rehanajaib04-commits <rehanajaib04@gmail.com>
2025-11-07 17:10:22 +0000

    act3

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator 
-      initialRouteName="ModuleAddScreen"
+      initialRouteName="ModuleListScreen"
       screenOptions={{
         headerStyle:{backgroundColor:'black'},
         headerTintColor:'white',
