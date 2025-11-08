f2769ae9d3f904c995625a372f1ab05be14aecbb
rehanajaib04-commits <rehanajaib04@gmail.com>
2025-11-08 16:47:22 +0000

    act5 (#7)

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -1,36 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import {StyleSheet, ScrollView, } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, ScrollView } from 'react-native';
 import Screen from '../layout/Screen';
 import initialModules from '../../data/modules.js';
+//import RenderCount from '../entity/ui/rendercount.js';
 import ModuleList from '../entity/modules/ModuleList.js';
 
 const ModuleListScreen = () => {
-  // Initialisation
-  const modules = initialModules; 
-//state
- //handlers
-    const handleSelect = (module) => alert(`Item ${module.ModuleCode} selected`); 
- //view 
+  // Initialization
+  // state
+  const [modules, setModules] = useState(initialModules);
 
+  // handlers
+  const handleDelete = (module) => 
+    setModules(modules.filter((item) => item.ModuleID !== module.ModuleID));
 
+  // view
   return (    
     <Screen>
-  <ModuleList modules={modules} OnSelect = {handleSelect}/>
+      <ModuleList modules={modules} OnSelect={handleDelete} /> 
     </Screen>
   );
 };
 
-const styles = StyleSheet.create({
-  container: { 
-  },
-  item: { 
-    paddingVertical: 15,
-    borderTopWidth: 1,
-    borderColor: 'lightgrey',
-  },
-  text: { 
-    fontSize: 16,
-  },
-}); 
 
 export default ModuleListScreen;
