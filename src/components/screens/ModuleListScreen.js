a8ee28586a1bfd9236264d16c1f9ea07351e8e62
rehanajaib04-commits <rehanajaib04@gmail.com>
2025-11-14 15:11:42 +0000

    Activity-08

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { StyleSheet, ScrollView } from 'react-native';
+import { LogBox, StyleSheet, ScrollView } from 'react-native';
 import Screen from '../layout/Screen';
 import initialModules from '../../data/modules.js';
 import RenderCount from '../entity/ui/rendercount.js';
@@ -7,14 +7,20 @@ import ModuleList from '../entity/modules/ModuleList.js';
 
 const ModuleListScreen = ({ navigation }) => { 
   // Initialization
+  LogBox.ignoreLogs(['nonserialized values were found in the navigation state'])
   // state
   const [modules, setModules] = useState(initialModules);
 
   // handlers
-  const handleSelect = (module) => navigation.navigate('ModuleViewScreen', { module });
+const handleSelect = (module) => 
+  navigation.navigate('ModuleViewScreen', { module, onDelete });
   const handleDelete = (module) => 
     setModules(modules.filter((item) => item.ModuleID !== module.ModuleID));
+const onDelete = (module) => {
+handleDelete(module);
+navigation.goBack();
 
+}
   // view
   return (    
     <Screen>
