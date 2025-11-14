999e119b57abaf882622c27b27f301e4adc0a272
rehanajaib04-commits <rehanajaib04@gmail.com>
2025-11-14 16:06:10 +0000

    Activity-09

--- src/components/screens/ModuleAddScreen.js
+++ src/components/screens/ModuleAddScreen.js
@@ -1,11 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
+import icons from '../../UI/icons.js';
+import { Button, ButtonTray } from '../../UI/Button.js';
 import { StyleSheet, Text, View } from 'react-native';
 import Screen from '../layout/Screen';
-
-const ModuleAddScreen = () => {
+ const defaultMode = {
+    ModuleID: 106778,
+    ModuleName: 'Games Programming',
+    ModuleCode: 'CI2270',
+    ModuleLevel: 4,
+    ModuleLeaderID: 1,
+    ModuleLeaderName: 'Graeme Jones',
+    ModuleImage:
+      'https://images.freeimages.com/images/small-previews/9b8/electronic-components-2-1242738.jpg',
+  };
+const ModuleAddScreen = ({ navigation, route }) => {
+  
+ // Initialization
+ const  {onAdd} = route.params;
+  // state
+  // handlers
+  const handleAdd = () => onAdd(defaultMode);
+  const handleCancel =() => navigation.goBack;
+   // view
   return (
     <Screen>
       <Text>Add</Text>
+      <ButtonTray>
+            <Button label="Add" icon={<icons.Add/>} onClick={handleAdd} />
+            <Button label="Cancel"  onClick={handleCancel} />
+             </ButtonTray>
     </Screen>
   );
 };
--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -4,6 +4,8 @@ import Screen from '../layout/Screen';
 import initialModules from '../../data/modules.js';
 import RenderCount from '../entity/ui/rendercount.js';
 import ModuleList from '../entity/modules/ModuleList.js';
+import icons from '../../UI/icons.js';
+import { Button, ButtonTray } from '../../UI/Button.js';
 
 const ModuleListScreen = ({ navigation }) => { 
   // Initialization
@@ -12,7 +14,7 @@ const ModuleListScreen = ({ navigation }) => {
   const [modules, setModules] = useState(initialModules);
 
   // handlers
-const handleSelect = (module) => 
+const gotoViewScreen = (module) => 
   navigation.navigate('ModuleViewScreen', { module, onDelete });
   const handleDelete = (module) => 
     setModules(modules.filter((item) => item.ModuleID !== module.ModuleID));
@@ -21,11 +23,20 @@ handleDelete(module);
 navigation.goBack();
 
 }
+const handleAdd = (module) => setModules ([...modules,module]);
+const onAdd = (module) => {
+handleAdd(module);
+navigation.goBack();
+}
+const gotoAddScreen = () => navigation.navigate('ModuleAddScreen',{onAdd});
   // view
   return (    
     <Screen>
       <RenderCount/>
-      <ModuleList modules={modules} OnSelect={handleSelect} /> 
+      <ButtonTray>
+      <Button label="Add" icon={<icons.Add/>} onClick={gotoAddScreen} />
+       </ButtonTray>
+             <ModuleList modules={modules} OnSelect={gotoViewScreen} /> 
     </Screen>
   );
 };
