0d3b4c923240d167e11fb6b1dcdbd27661491da0
rehanajaib04-commits <rehanajaib04@gmail.com>
2025-11-15 17:38:58 +0000

    activity 11

---
+++ src/components/entity/modules/ModuleForm.js
@@ -0,0 +1,106 @@
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import icons from '../../../UI/icons.js';
+import Form from '../../../UI/Form.js';
+
+const defaultModule = {
+  ModuleID: null, // 106778,
+  ModuleName: null, // 'Games Programming',
+  ModuleCode: null, // 'CI2270',
+  ModuleLevel: null, // 4,
+  ModuleLeaderID: null, // 1,
+  ModuleLeaderName: null, // 'Graeme Jones',
+  ModuleImage: null, 
+};
+
+const ModuleForm = ({ onCancel, onSubmit }) => { 
+  // Initializations ---------------------
+  defaultModule.ModuleID = Math.floor(100000 + Math.random() * 9000000);
+  defaultModule.ModuleImage = 'https://images.freeimages.com/images/small-previews/9b8/electronic-components-2-1242738.jpg';
+
+  const levels = [
+    {value: 3, label : '3 (foundaiton)' },
+    {value: 4, label : '4 (first year)' },
+    {value: 5, label : '5 (second year)' },
+    {value: 6, label : '6 (third year)' },
+    {value: 7, label : '7 (masters)' },
+
+
+
+
+
+
+  ]
+
+  // State -------------------------------
+  const [module, setModule] = useState(defaultModule);
+
+  // Handlers ----------------------------
+  const handleChange = (field, value) => setModule({ ...module, [field]: value });
+  const handleSubmit = () => onSubmit(module); 
+
+  // View --------------------------------
+  const submitLabel = "Add";
+  const submitIcon = <icons.Add />;
+
+  return (
+    <Form
+      onSubmit={handleSubmit}
+      onCancel={onCancel}
+      submitLabel={submitLabel}
+      submitIcon={submitIcon}
+    >
+      <Form.InputText
+        label="ModuleCode"
+        value={module.ModuleCode}
+        onChangeText={(value) => handleChange('ModuleCode', value)}
+        style={styles.itemTextInput}
+      />
+      <Form.InputText
+        label="ModuleName"
+        value={module.ModuleName}
+        onChangeText={(value) => handleChange('ModuleName', value)}
+        style={styles.itemTextInput}
+      />
+      <Form.InputText
+        label="ModuleLeaderName"
+        value={module.ModuleLeaderName}
+        onChangeText={(value) => handleChange('ModuleLeaderName', value)}
+        style={styles.itemTextInput}
+      />
+      <Form.InputSelect
+        label="ModuleLevel"
+        prompt= "Select module level..."
+        options={levels}
+        value={module.ModuleLevel}
+        onChangeText={(value) => handleChange('ModuleLevel', value)}
+        style={styles.itemTextInput}
+      />
+      <Form.InputText
+        label="ModuleImage"
+        value={module.ModuleImage}
+        onChangeText={(value) => handleChange('ModuleImage', value)}
+        style={styles.itemTextInput}
+      />
+    </Form>
+  );
+};
+
+const styles = StyleSheet.create({
+  itemLabel: {
+    color: 'grey',
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  itemTextInput: {
+    height: 50,
+    paddingLeft: 10,
+    fontSize: 16,
+    backgroundColor: 'white',
+    borderRadius: 7,
+    borderWidth: 1,
+    borderColor: 'lightgrey',
+  },
+});
+
+export default ModuleForm;
--- src/components/screens/ModuleAddScreen.js
+++ src/components/screens/ModuleAddScreen.js
@@ -1,89 +1,30 @@
 import { useState } from 'react';
-import icons from '../../UI/icons.js';
-import { Button, ButtonTray } from '../../UI/Button.js';
 import { StyleSheet, Text, View, TextInput} from 'react-native';
 import Screen from '../layout/Screen';
- const defaultModule = {
-    ModuleID: null,// 106778,
-    ModuleName:null,// 'Games Programming',
-    ModuleCode: null,//'CI2270',
-    ModuleLevel:null,// 4,
-    ModuleLeaderID: null,//1,
-    ModuleLeaderName: null,// 'Graeme Jones',
-    ModuleImage:null,
-    //'https://images.freeimages.com/images/small-previews/9b8/electronic-components-2-1242738.jpg',
-  };
+import ModuleForm from '../entity/modules/ModuleForm';
+
 const ModuleAddScreen = ({ navigation, route }) => {
   
  // Initialization
  const  {onAdd} = route.params;
-defaultModule.ModuleID = Math.floor(100000 + Math.random() * 9000000),
-defaultModule.ModuleImage = 'https://images.freeimages.com/images/small-previews/9b8/electronic-components-2-1242738.jpg';
+
 
   // state
-  const [module,setModule] = useState(defaultModule);
+
   // handlers
-  const handleAdd = () => onAdd(module);
-  const handleCancel =() => navigation.goBack;
-  const HandleChange = (field,value) => setModule({...module, [field]:value});
+const handleCancel = () => navigation.goBack();
+
+
    // view
   return (
     <Screen>
-      <View style={styles.item}>
-        <Text style={styles.itemLabel}>Module Code</Text>
-        <TextInput value = {module.ModuleCode}  onChangeText={(value) => HandleChange ('ModuleCode',value)}style = {styles.itemTextInput}/>
-      </View>
-       
-       <View style={styles.item}>
-        <Text style={styles.itemLabel}>Module name</Text>
-        <TextInput value = {module.ModuleName}  onChangeText={(value) => HandleChange ('ModuleName',value)}style = {styles.itemTextInput}/>
-      </View>
-
-      <View style={styles.item}>
-        <Text style={styles.itemLabel}>Module leader</Text>
-        <TextInput value = {module.ModuleLeaderName}  onChangeText={(value) => HandleChange ('ModuleLeaderName',value)}style = {styles.itemTextInput}/>
-      </View>
-
-
-
-      <View style={styles.item}>
-        <Text style={styles.itemLabel}>Module level</Text>
-        <TextInput value = {module.ModuleLevel}  onChangeText={(value) => HandleChange ('ModuleLevel',value)}style = {styles.itemTextInput}/>
-      </View>
-
-
-      <View style={styles.item}>
-        <Text style={styles.itemLabel}>Module image url</Text>
-        <TextInput value = {module.ModuleImage}  onChangeText={(value) => HandleChange ('ModuleImage',value)}style = {styles.itemTextInput}/>
-      </View>
-
-
-
-      
-      <Text>Add</Text>
-      <ButtonTray>
-            <Button label="Add" icon={<icons.Add/>} onClick={handleAdd} />
-            <Button label="Cancel"  onClick={handleCancel} />
-             </ButtonTray>
+      <ModuleForm onCancel ={handleCancel}  onSubmit ={onAdd}/>
     </Screen>
   );
 };
 
 const styles = StyleSheet.create({
-  itemLabel:{
-color:'grey',
-fontSize: 16,
-marginBottom: 5,
-  },
-  itemTextInput:{
-height: 50,
-paddingLeft:10,
-fontsize:16,
-backgroundColor:'white',
-borderRadius:7,
-borderWidth:1,
-borderColor:'lightgrey',
-  },
+ 
   });
 
 export default ModuleAddScreen;
